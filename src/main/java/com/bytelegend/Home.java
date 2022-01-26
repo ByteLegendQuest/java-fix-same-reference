896dea1fba08929be89e23b6f32109dbfbe4c089
o0hana <2487311872@qq.com>
2022-01-26 06:35:02 +0000

    Update at 2022-01-26T06:35:02.593248Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -8,7 +8,7 @@ public class Home {
     public static Cat cat3;
 
     public static void main(String[] args) {
-        cat1 = new Cat();
+        Cat cat1 = new Cat();
         cat2 = cat1;
         cat3 = cat2;
         System.out.println(cat1);
