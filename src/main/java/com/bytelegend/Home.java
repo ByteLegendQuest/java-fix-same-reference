3650cdb464ba53a09c597aba35d1e037f43b301b
lan-lulu <742764356@qq.com>
2022-12-13 03:48:30 +0000

    Update at 2022-12-13T03:48:30.247249Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -6,11 +6,11 @@ public class Home {
     public static Cat cat1;
     public static Cat cat2;
     public static Cat cat3;
-
+    
     public static void main(String[] args) {
         cat1 = new Cat();
-        cat2 = cat1;
-        cat3 = cat2;
+        cat2 = new Cat();
+        cat3 = new Cat();
         System.out.println(cat1);
         System.out.println(cat2);
         System.out.println(cat3);
